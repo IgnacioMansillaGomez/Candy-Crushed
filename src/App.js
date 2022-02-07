c844017e9371f74408c16b90181e9af3623d11f1
Ignacio <papelgola16@gmail.com>
2022-02-07 02:00:26 -0300

    board and three column check added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,23 @@ export const App = () => {
   const width = 8; // eight squares width
   const colors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
+  const checkForColumnOfThree = () => {
+    for (let i = 0; i < 47; i++) {
+      const columnOfThree = [i, i + width, i + width * 2];
+      const decidedColor = currentColorArrangement[i];
+
+      if (
+        columnOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        columnOfThree.forEach(
+          (result) => (currentColorArrangement[result] = "")
+        );
+      }
+    }
+  };
+
   const createBoard = () => {
     // color between 0 and 5
     // const randomNumber0to5 = Math.floor(Math.random() * colors.length);
@@ -20,13 +37,25 @@ export const App = () => {
 
   useEffect(() => {
     createBoard();
-  }, []);
 
-  console.log(currentColorArrangement);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      checkForColumnOfThree();
+      setCurrentColorArrangement([...currentColorArrangement]);
+    }, 100);
+    return () => clearInterval(timer);
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [checkForColumnOfThree]);
 
   return (
-    <>
-      <p>asdasd</p>
-    </>
+    <div className="app">
+      <div className="game">
+        {currentColorArrangement.map((element, index) => (
+          <img key={index} alt={element} style={{ backgroundColor: element }} />
+        ))}
+      </div>
+    </div>
   );
 };
