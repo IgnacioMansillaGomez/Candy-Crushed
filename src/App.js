32903ab79c30d0c1cead8f39bc5fc6676e9c29a1
Ignacio <papelgola16@gmail.com>
2022-02-18 09:19:04 -0300

    images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import ScoreBoard from "./components/ScoreBoard";
+import blueCandy from "./images/blue-candy.png";
+import greenCandy from "./images/green-candy.png";
+import orangeCandy from "./images/orange-candy.png";
+import purpleCandy from "./images/purple-candy.png";
+import redCandy from "./images/red-candy.png";
+import yellowCandy from "./images/yellow-candy.png";
+import blank from "./images/blank.png";
 
 export const App = () => {
   const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
