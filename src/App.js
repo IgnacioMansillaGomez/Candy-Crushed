9660f868ff4646562c9eb4e9b8490d154bf8ee0b
Ignacio <papelgola16@gmail.com>
2022-02-18 09:13:59 -0300

    check for rowthree-four and columnfour-three

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ export const App = () => {
   const width = 8; // eight squares width
   const colors = ["blue", "green", "orange", "purple", "red", "yellow"];
 
+  const checkForColumnOfFour = () => {
+    for (let i = 0; i <= 39; i++) {
+      const columnOfFour = [i, i + width, i + width * 2, i + width * 3];
+      const decidedColor = currentColorArrangement[i];
+      const isBlank = currentColorArrangement[i] === blank;
+
+      if (
+        columnOfFour.every(
+          (square) =>
+            currentColorArrangement[square] === decidedColor && !isBlank
+        )
+      ) {
+        setScoreDisplay((score) => score + 4);
+        columnOfFour.forEach(
+          (square) => (currentColorArrangement[square] = blank)
+        );
+        return true;
+      }
+    }
+  };
+
   const checkForColumnOfThree = () => {
     for (let i = 0; i < 47; i++) {
       const columnOfThree = [i, i + width, i + width * 2];
@@ -17,8 +38,50 @@ export const App = () => {
         )
       ) {
         columnOfThree.forEach(
-          (result) => (currentColorArrangement[result] = "")
+          (square) => (currentColorArrangement[square] = "")
         );
+      }
+    }
+  };
+
+  const checkForRowOfFour = () => {
+    for (let i; i < 64; i++) {
+      // 0 ,8 ,16 ,24
+      const rowOfFour = [i, i + 1, i + 2, i + 3];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+        54, 55, 62, 63, 64,
+      ];
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfFour.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfFour.forEach((square) => (currentColorArrangement[square] = ""));
+      }
+    }
+  };
+
+  const checkForRowOfThree = () => {
+    for (let i = 0; i < 64; i++) {
+      const rowOfThree = [i, i + 1, i + 2];
+      const decidedColor = currentColorArrangement[i];
+      const notValid = [
+        6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64,
+      ];
+
+      if (notValid.includes(i)) continue;
+
+      if (
+        rowOfThree.every(
+          (square) => currentColorArrangement[square] === decidedColor
+        )
+      ) {
+        rowOfThree.forEach((square) => (currentColorArrangement[square] = ""));
       }
     }
   };
@@ -42,18 +105,27 @@ export const App = () => {
   }, []);
   useEffect(() => {
     const timer = setInterval(() => {
+      checkForColumnOfFour();
       checkForColumnOfThree();
+      checkForRowOfFour();
+      checkForRowOfThree();
       setCurrentColorArrangement([...currentColorArrangement]);
     }, 100);
     return () => clearInterval(timer);
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [checkForColumnOfThree]);
+  }, [
+    checkForColumnOfFour,
+    checkForColumnOfThree,
+    checkForRowOfFour,
+    checkForRowOfThree,
+    currentColorArrangement,
+  ]);
 
   return (
     <div className="app">
       <div className="game">
         {currentColorArrangement.map((element, index) => (
-          <img key={index} alt={element} style={{ backgroundColor: element }} />
+          <img key={index} style={{ backgroundColor: element }} />
         ))}
       </div>
     </div>
